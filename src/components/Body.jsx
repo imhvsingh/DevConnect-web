b0568b918208a1b6ad5fa8297dff390beaeaeab1
Harsh Vardhan Singh <hvsingh5732@gmail.com>
2025-03-22 13:03:58 +0530

    Fixed Connection Request

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,29 +1,38 @@
-import { Outlet } from "react-router-dom";
+import { useNavigate, Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../utils/userSlice";
 import { useEffect } from "react";
 
 const Body = () => {
   const dispatch = useDispatch();
+  const userData = useSelector((store) => store.user);
+  const navigate = useNavigate();
+ 
   const fetchUser = async () => {
+    if(userData) return;
     try {const res = await axios.get( BASE_URL + "/profile/view", {
       withCredentials: true,
     });
     dispatch(addUser(res.data));
   }
 
-    catch (err) {
-      console.log(err);
+  catch (error) {
+    if(error.status === 401){
+      navigate("/login")
+    }
+    //console.error(error)
     }
   }
 
 useEffect(() => {
+  
   fetchUser();
-})
+  
+},[]);
 
   return (
     <div> 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,15 @@
 import { useSelector } from "react-redux";
+import EditProfile from "./EditProfile";
 
-const Profile = () => {
-  const user = useSelector((store) => store.user);
-  return (
-    user && (
-      <div>
-        <EditProfile user={user} />
-      </div>
+const Profile=()=>{
+    const user=useSelector(store=>store.user);
+    return( 
+        user&&(
+        <div className="p-5">
+            <EditProfile user={user}/>
+        </div>
+        )
     )
-  );
-};
+}
+
 export default Profile;
